6b5342e1d052daa055419b6855781c99f5cd2141
gwss <1stapletolex@hdsb.ca>
2021-11-24 11:04:01 -0500

    Update MulticolorMAX7219.ino

--- DigitalChessBoard/MulticolorMAX7219/MulticolorMAX7219.ino
+++ DigitalChessBoard/MulticolorMAX7219/MulticolorMAX7219.ino
@@ -7,10 +7,11 @@
 // prescaler is /128 -  125,000/ISR_FREQ+1 (i.e 249=500Hz, 190=650Hz)
 // Tweaked depending on the overhead in the ISR & and other factors in the sketch
 // Display will be slow if too FAST. Sketch will hang on delay() if way too fast!
- 
+
+#define BLUE 2
 #define GREEN 1                         // The address of the MAX7221 for the green leds
-#define RED 2                           // The address of the MAX7221 for the red leds
-int maxInShutdown=GREEN;                // tells which MAX7221 is currently off
+#define RED 0                           // The address of the MAX7221 for the red leds
+int activeMax=GREEN;                // tells which MAX7221 is currently off
 unsigned long ISRTime;                   // DEBUG to test how long in ISR
  
 LedControl lc=LedControl(12,11,10,3); // pins 10=DataIn, 9=CLK, 8=LOAD + 2 MAX7221s
@@ -25,19 +26,8 @@ void setup() {
   lc.setIntensity(GREEN,15);            // 0 = dim, 15 = full brightness
   lc.setIntensity(RED,15);              // red needs less brightness
   setISRtimer();                        // setup the timer
-  //startISR();                           // start the timer to toggle shutdown
-  for(int row=0; row != 8;row++) {
-   for(int i =0; i != 8; i += 2){
-    if(row % 2 ==0){
-      SetLed(GREEN, row, i, true);
-      SetLed(RED, row, i + 1, true);
-    }
-    else{
-      SetLed(RED, row, i, true);
-      SetLed(GREEN, row, i + 1, true);
-    }
-   }
-  }
+                          // start the timer to toggle shutdown
+  
   
   for(int i = 0; i != 2; i++){
     pinMode(power_pins[i], OUTPUT);
@@ -49,19 +39,19 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("Starting");
+  startISR();   
 }
 
 String command = "";
 void loop() {
-  stopISR();
+  /*
+   stopISR();
   if(Serial.available()){
-    
+   
     command = Serial.readString();
     command.trim();
     Serial.println(command);
     if(command == "rb"){
-      Serial.println("Reading Board");
-      Serial.println("-------------");
       CheckBoard();
     }
     else if(command == "wc"){
@@ -69,11 +59,29 @@ void loop() {
     }
 
     command = "";
-    
+   
   }
-  //startISR();
   
-  delay(10);
+  startISR();
+  */
+   stopISR();
+  
+   for(int row=0; row != 8;row++) {
+   for(int i =0; i != 8; i += 2){
+    if(row % 2 ==0){
+      SetLed(GREEN, row, i, true);
+      SetLed(BLUE, row, i + 1, true);
+    }
+    else{
+      SetLed(BLUE, row, i, true);
+      SetLed(GREEN, row, i + 1, true);
+    }
+   }
+  }
+  
+  startISR(); 
+  
+  delay(1000);
 }
 ///////////////////////////Sensor Grid Functions ///////////////////////////
 void CheckBoard(){
@@ -96,15 +104,19 @@ void SetColumnPower(int col){
  
 /////////////////////////////ISR Timer Functions ///////////////////////////
 ISR(TIMER2_COMPA_vect) {  //This ISR toggles shutdown between the 2MAX7221's
-  if(maxInShutdown==RED){
-    lc.shutdown(GREEN,true);  // The order here is critical - Shutdown first!
-    lc.shutdown(RED,false);   // . . . Then restart the other.
-    maxInShutdown=GREEN;
+  if(activeMax==RED){
+    lc.shutdown(RED,true);  // The order here is critical - Shutdown first!
+    lc.shutdown(GREEN, false);
+    activeMax=GREEN;
   }
-  else {
-    lc.shutdown(RED,true);
-    lc.shutdown(GREEN,false);
-    maxInShutdown=RED;
+  else if(activeMax==GREEN){
+    lc.shutdown(GREEN,true);  // The order here is critical - Shutdown first!
+    lc.shutdown(BLUE, false);
+    activeMax=BLUE;
+  } else {
+    lc.shutdown(BLUE,true);   // . . . Then restart the other.
+    lc.shutdown(RED, false);
+    activeMax=RED;
   }
 }  
  
